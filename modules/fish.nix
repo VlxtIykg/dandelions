ae23225fd5a91a7df65facef2858a9eb1180cd04
kami <97310758+VlxtIykg@users.noreply.github.com>
2026-04-06 14:09:01 +0800

    feat: fish update
    Updated paths, updated
    shellInit, set paths for npm,
    rebind control z to reopen
    editor (fg job), changed
    alias hx to vim

--- modules/fish.nix
+++ modules/fish.nix
@@ -6,7 +6,7 @@
 }:
 let
   cfg = config.destiny.programs.fish;
-  path = "/etc/nixos/dandelions/";
+  path = "/data/dandelions/";
 
   inherit (config.destiny.flake.system) user;
   inherit (lib.options) mkEnableOption;
@@ -20,7 +20,8 @@ in
     home-manager.users.${user} = {
       programs.fish = {
         enable = true;
-        shellInit = "direnv hook fish | source";
+        # set -x PATH "$HOME/.npm_packages/bin/" $PATH
+        shellInit = "export DIRENV_LOG_FORMAT=;direnv hook fish | source";
         interactiveShellInit = builtins.concatStringsSep "\n" [
           ''
             function fish_greeting
@@ -31,11 +32,16 @@ in
             end
 
             set -g fish_greeting
-            set -x PATH "$HOME/.npm_packages/bin/" $PATH
             set -x DISPLAY ":0.0"
+            fish_add_path --append --path /home/kami/.bun/bin
+            fish_add_path --append --path /data
           ''
           (builtins.readFile ../assets/prompt.fish)
         ];
+
+        binds = {
+          "ctrl-z".command = "fg 2>/dev/null; commandline -f repaint";
+        };
 
         shellAbbrs = {
           projs = "cd /data/db4/Github";
@@ -47,23 +53,23 @@ in
           cmtest = "cargo miri test";
           cchck = "cargo check";
           g = "git";
-          v = "hx";
+          v = "vim";
           ":q" = "exit";
           gcmt = "git commit -m";
           gpsh = "git push";
           nxsh = "nix-shell -p";
-          proj = "hx /data/db4/Github/";
-          nixcfg = "hx ${path}";
-          nxr = "sudo nixos-rebuild switch --flake ${path}#radio"; # Git add required to proceed to boot
-          nxrtest = ''
-            bash ${path}lib/updateNixos || bash ~/updateNixos
-          ''; # Will update the system on hand. Use this till sure won't break
-          snooze = "poweroff";
-          phoenix = "sudo reboot now";
+          nxshellify = "nix-shellify -p";
+          proj = "vim /data/db4/Github/";
+          nixcfg = "vim ${path}";
+          # nxr = "sudo nixos-rebuild switch --flake ${path}#radio"; # Git add required to proceed to boot
+          nxr = "bash ${path}lib/upgradeNixos"; # Doesn't require git like the above line. Reason being the script just copies over
+          nxrtest = "bash ${path}lib/updateNixos"; # Will update the system on hand. Use this till sure won't break
+          snooze = "${pkgs.systemd}/bin/poweroff";
+          phoenix = "${pkgs.systemd}/bin/reboot";
           screensaver = "nix run nixpkgs#nyancat";
           clear = "nix run nixpkgs#nyancat -- -f 20";
           catto = "nix run nixpkgs#nyancat";
-          what2do2day = "hx /data/db4/Github/todo_list.txt";
+          what2do2day = "vim /data/db4/Github/todo_list.txt";
         };
 
         plugins = [
