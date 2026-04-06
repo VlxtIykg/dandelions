ad982ecbf1410e4d867f82a454ffb06b2f1fdc04
kami <97310758+VlxtIykg@users.noreply.github.com>
2026-04-06 14:00:37 +0800

    Security update
    No sudo required to reboot or
    poweroff

--- hosts/radio/configuration.nix
+++ hosts/radio/configuration.nix
@@ -60,11 +60,44 @@
     systemPackages = import ./packages.nix { inherit inputs pkgs self; };
   };
 
-  security.polkit.enable = true;
+  security = {
+    polkit.enable = true;
+    sudo = {
+      enable = true;
+      extraRules = [
+        {
+          commands = [
+            {
+              command = "${pkgs.systemd}/bin/systemctl suspend";
+              options = [ "NOPASSWD" ];
+            }
+            {
+              command = "${pkgs.systemd}/bin/reboot";
+              options = [ "NOPASSWD" ];
+            }
+            {
+              command = "${pkgs.systemd}/bin/poweroff";
+              options = [ "NOPASSWD" ];
+            }
+          ];
+          groups = [ "wheel" ];
+        }
+      ];
+
+      extraConfig = with pkgs; ''
+        Defaults:picloud secure_path="${
+          lib.makeBinPath [
+            systemd
+          ]
+        }:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin"
+      '';
+    };
+  };
 
   nixpkgs.config = {
     allowUnfree = true;
     pulseaudio = true;
+    permittedInsecurePackages = [ "olm-3.2.16" ];
   };
 
   programs = {
